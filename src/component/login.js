fc2221378cb36cb920edb98befb3d4cab3e2e700
usman <5935512036@psu.ac.th>
2020-03-23 01:15:47 +0700

    fristcreatebackend

---
+++ src/component/login.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+
+const LoginFrom = () =>{
+    const [ formdata ,setformdata ] = useState({
+        userid: '',
+        password: ''
+    })
+
+    return(
+        <div>gfgh</div>
+    )
+
+
+
+
+
+
+
+
+}
+export default LoginFrom ;
